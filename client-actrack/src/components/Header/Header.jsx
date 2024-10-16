814b917b258a796c498cd5542085c73908c9ec66
yvonneitan <itan3040@mylaurier.ca>
2024-10-16 15:32:29 -0700

    Update the link of profile button

--- client-actrack/src/components/Header/Header.jsx
+++ client-actrack/src/components/Header/Header.jsx
@@ -4,38 +4,47 @@ import { useNavigate } from "react-router-dom";
 import { handleNav } from "../../utils/utils";
 
 function Header() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const isActive = (path) => location.pathname === path;
 
   return (
     <header className="header">
-      <p className="header__logo" onClick={() => handleNav(navigate, "/")}>AcTrack</p>
+       <p
+          className={`header__logo ${isActive('/') ? 'active' : ''}`}
+          onClick={() => handleNav(navigate, '/')}>AcTrack
+        </p>
+     
       <div className="header__container">
         <p
-          className={`header__home ${isActive('/') ? 'active' : ''}`}
-          onClick={() => handleNav(navigate, '/')}>Home
+          className={`header__home ${isActive("/") ? "active" : ""}`}
+          onClick={() => handleNav(navigate, "/")}
+        >
+          Home
         </p>
         <p
-          className={`header__lists ${isActive('/lists') ? 'active' : ''}`}
-          onClick={() => handleNav(navigate, '/lists')}>Lists
+          className={`header__lists ${isActive("/lists") ? "active" : ""}`}
+          onClick={() => handleNav(navigate, "/lists")}
+        >
+          Lists
         </p>
-      
+
         <p
-          className={`header__completed ${isActive('/completed') ? 'active' : ''}`}
-          onClick={() => handleNav(navigate, '/completed')}>Completed
+          className={`header__completed ${
+            isActive("/completed") ? "active" : ""
+          }`}
+          onClick={() => handleNav(navigate, "/completed")}
+        >
+          Completed
         </p>
-
-        
-
       </div>
 
       <div className="header__profile">
-      
         <p
-          className={`header__profile-icon ${isActive('/') ? 'active' : ''}`}
-          onClick={() => handleNav(navigate, '/')}>Yvonne
+          className={`header__profile-icon ${isActive("/") ? "active" : ""}`}
+          onClick={() => handleNav(navigate, "/")}
+        >
+          Yvonne
         </p>
-
       </div>
     </header>
   );
