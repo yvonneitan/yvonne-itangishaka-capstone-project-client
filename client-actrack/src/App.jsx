b3dcc9bab2c06ee3882abc6e7ce53235bddaebfc
yvonneitan <itan3040@mylaurier.ca>
2024-10-16 11:23:24 -0700

    Add a Header Component

--- client-actrack/src/App.jsx
+++ client-actrack/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.scss'
+import Header from "./components/Header/Header"
 
 function App() {
   return (
     <>
       
-      <h1>Welcome to AcTrack</h1>
+     <Header/>
     </>
   )
 }
---
+++ client-actrack/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+         <h1>Welcome to AcTrack</h1>
+      
+    </div>
+  )
+}
+
+export default Header
