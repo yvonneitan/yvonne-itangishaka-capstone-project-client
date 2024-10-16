2ee5f5dc7d72e5f4ad1c22bdf0504c99d51a1903
yvonneitan <itan3040@mylaurier.ca>
2024-10-16 15:18:26 -0700

    Update the header links to navigate to respective page and change link color accordingly

--- client-actrack/src/components/Header/Header.jsx
+++ client-actrack/src/components/Header/Header.jsx
@@ -4,21 +4,38 @@ import { useNavigate } from "react-router-dom";
 import { handleNav } from "../../utils/utils";
 
 function Header() {
-  const navigate = useNavigate(); // Get the navigate function from useNavigate hook
+  const navigate = useNavigate(); 
+  const isActive = (path) => location.pathname === path;
 
   return (
     <header className="header">
-      <p className="header__logo" onClick={() => handleNav(navigate, "/")}>
-        AcTrack
-      </p>
+      <p className="header__logo" onClick={() => handleNav(navigate, "/")}>AcTrack</p>
       <div className="header__container">
-        <p className="header__home" onClick={() => handleNav(navigate, "/")}>Home</p>
-        <p className="header__lists" onClick={() => handleNav(navigate, "/lists")} >Lists</p>
-        <p className="header__completed"onClick={() => handleNav(navigate, "/completed")}>Completed</p>
+        <p
+          className={`header__home ${isActive('/') ? 'active' : ''}`}
+          onClick={() => handleNav(navigate, '/')}>Home
+        </p>
+        <p
+          className={`header__lists ${isActive('/lists') ? 'active' : ''}`}
+          onClick={() => handleNav(navigate, '/lists')}>Lists
+        </p>
+      
+        <p
+          className={`header__completed ${isActive('/completed') ? 'active' : ''}`}
+          onClick={() => handleNav(navigate, '/completed')}>Completed
+        </p>
+
+        
+
       </div>
 
       <div className="header__profile">
-        <p className="header__profile-icon">Yvonne</p>
+      
+        <p
+          className={`header__profile-icon ${isActive('/') ? 'active' : ''}`}
+          onClick={() => handleNav(navigate, '/')}>Yvonne
+        </p>
+
       </div>
     </header>
   );
