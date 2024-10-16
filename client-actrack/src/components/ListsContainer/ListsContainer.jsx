7a6f9f8046ab0299a0d4bb7b2e09c45382773999
yvonneitan <itan3040@mylaurier.ca>
2024-10-16 12:50:43 -0700

    Add ListsContainer component, Add fonts

---
+++ client-actrack/src/components/ListsContainer/ListsContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ListsContainer/ListsContainer'
+
+function ListsContainer() {
+  return (
+    <div className='lists'>
+        <p className="list__title">
+            Yvonne's AcTrack
+        </p>
+
+      
+    </div>
+  )
+}
+
+export default ListsContainer
