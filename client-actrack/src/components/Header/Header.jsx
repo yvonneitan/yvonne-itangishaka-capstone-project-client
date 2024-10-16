9c21984d252af460f65ba037dd3af2ea7e6cd29c
yvonneitan <itan3040@mylaurier.ca>
2024-10-16 15:00:26 -0700

    Add a navigation Functions

--- client-actrack/src/components/Header/Header.jsx
+++ client-actrack/src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import './Header.scss'
+import React from "react";
+import "./Header.scss";
+import { useNavigate } from "react-router-dom";
+import { handleNav } from "../../utils/utils";
 
 function Header() {
+  const navigate = useNavigate(); // Get the navigate function from useNavigate hook
+
   return (
-    <header className='header'>
-         <p className='header__logo'>AcTrack</p>
-         <div className="header__container">
-            <p className='header__home'>Home</p>
-            <p className='header__lists'>Lists</p>
-            <p className='header__completed'>Completed</p>
-         </div>
-         <div className="header__profile">
-            <p className="header__profile-icon">
-                Yvonne
-            </p>
-         </div>
-      
+    <header className="header">
+      <p className="header__logo" onClick={() => handleNav(navigate, "/")}>
+        AcTrack
+      </p>
+      <div className="header__container">
+        <p className="header__home" onClick={() => handleNav(navigate, "/")}>Home</p>
+        <p className="header__lists" onClick={() => handleNav(navigate, "/lists")} >Lists</p>
+        <p className="header__completed"onClick={() => handleNav(navigate, "/completed")}>Completed</p>
+      </div>
+
+      <div className="header__profile">
+        <p className="header__profile-icon">Yvonne</p>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
