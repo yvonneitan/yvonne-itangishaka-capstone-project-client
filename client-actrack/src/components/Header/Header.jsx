0c242b2a88f7d1dc953223165c3aae99dab861e9
yvonneitan <itan3040@mylaurier.ca>
2024-10-16 11:53:42 -0700

    Style Header

--- client-actrack/src/components/Header/Header.jsx
+++ client-actrack/src/components/Header/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import './Header.scss'
 
 function Header() {
   return (
-    <div>
-         <h1>Welcome to AcTrack</h1>
+    <header className='header'>
+         <p className='header__logo'>AcTrack</p>
+         <div className="header__container">
+            <p className='header__home'>Home</p>
+            <p className='header__lists'>Lists</p>
+            <p className='header__completed'>Completed</p>
+         </div>
+         <div className="header__profile">
+            <p className="header__profile-icon">
+                Yvonne
+            </p>
+         </div>
       
-    </div>
+    </header>
   )
 }
 
