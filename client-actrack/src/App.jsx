f174127749f0abd9defc585e5aeebb70427f3960
yvonneitan <itan3040@mylaurier.ca>
2024-10-16 09:59:49 -0700

    Add client files

---
+++ client-actrack/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import actrackLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Welcome to AcTrack</h1>
+    </>
+  )
+}
+
+export default App
