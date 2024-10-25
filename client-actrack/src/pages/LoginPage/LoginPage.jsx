a73b0f1dd2ddc2643122b2097acf6be3a324d930
yvonneitan <itan3040@mylaurier.ca>
2024-10-25 07:51:48 -0700

    Formart the login Page

--- client-actrack/src/pages/LoginPage/LoginPage.jsx
+++ client-actrack/src/pages/LoginPage/LoginPage.jsx
@@ -32,25 +32,31 @@ function LoginPage() {
       <form onSubmit={handleLogin} className="login-container__form">
         <div className='login-container__user'>
           <label className='login-container__label'>Username:</label>
+          <div className="login-container__user--items">
           <img src={userIcon} alt="" className="login-container__user--icon" />
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            placeholder="Type your user name"
             required
             className='login-container__user--name'
           />
+          </div>
         </div>
         <div className="login-container__password">
           <label className='login-container__label'>Password:</label>
+          <div className="login-container__password--items">
           <img src={passwordIcon} alt="" className="login-container__password--icon" />
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            placeholder="Type your user password"
             required
-            className='login-container__user--password'
+            className='login-container__password--text'
           />
+          </div>
         </div>
         <button type="submit" className="login-container__button">Login</button>
       </form>
