02d3e730f11b37d0335db2d7c5d4f3dc2c75dc86
yvonneitan <itan3040@mylaurier.ca>
2024-10-25 00:18:37 -0700

    Hide RightSideContainer on login Page

--- client-actrack/src/App.jsx
+++ client-actrack/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.scss';
 import Header from "./components/Header/Header";
 import ListsContainer from './components/ListsContainer/ListsContainer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ListsPage from './pages/ListsPage/ListsPage';
 import CompletedPage from './pages/CompletedPage/CompletedPage';
 import LoginPage from './pages/LoginPage/LoginPage';
-import GreetingsContainer from './components/GreetingsContainer/GreetingsContainer';
-import MiddleContainer from './components/MiddleContainer/MiddleContainer';
-import RightSideContainer from './components/RightSideContainer/RightSideContainer';
+// import GreetingsContainer from './components/GreetingsContainer/GreetingsContainer';
+// import MiddleContainer from './components/MiddleContainer/MiddleContainer';
+// import RightSideContainer from './components/RightSideContainer/RightSideContainer';
+import ShowRightSideContainer from './components/ShowRightSideContainer/ShowRightSideContainer';
 
 function App() {
   return (
@@ -21,10 +22,11 @@ function App() {
           <Route path="/lists" element={<ListsPage />} /> 
           <Route path="/completed" element={<CompletedPage />} />
         </Routes>
-        <RightSideContainer/>
+      <ShowRightSideContainer/>
       </div>
     </BrowserRouter>
-  )
+  );
 }
+
 
 export default App;
---
+++ client-actrack/src/components/ShowRightSideContainer/ShowRightSideContainer.jsx
@@ -0,0 +1,14 @@
+// src/components/ShowRightSideContainer.jsx
+
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import RightSideContainer from "../RightSideContainer/RightSideContainer";
+
+function ShowRightSideContainer() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/";
+
+  return !isLoginPage && <RightSideContainer />;
+}
+
+export default ShowRightSideContainer;
