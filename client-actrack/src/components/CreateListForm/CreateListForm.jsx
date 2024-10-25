2c4c7df5f889abeda98b5992d090bc1140cc2009
yvonneitan <itan3040@mylaurier.ca>
2024-10-25 11:27:58 -0700

    Add a create List form and its functionalities

--- client-actrack/src/components/CreateListForm/CreateListForm.jsx
+++ client-actrack/src/components/CreateListForm/CreateListForm.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import errorIcon from "../../assets/icons/error-24px.svg";
 
-function CreateListForm() {
+function CreateListForm({ newListName, onInputChange, onCreateList, onCancel, inputError }) {
   return (
-    <div>
-      
+    <div className="create__box">
+      <div className="sidebar__create">
+        <input
+          type="text"
+          value={newListName}
+          onChange={onInputChange}
+          placeholder="New List Name"
+          className="sidebar__create--list-input"
+        />
+        {inputError && (
+          <div className="sidebar__create--error">
+            <img src={errorIcon} alt="Warning Icon" className="sidebar__create--error-icon" />
+            <div className="sidebar__create--error-message">List name cannot be Empty!</div>
+          </div>
+        )}
+        <div className="sidebar__create--buttons">
+          <button className="sidebar__create--list-add-btn" onClick={onCreateList}>Add</button>
+          <button type="button" className="sidebar__create--list-cancel-btn" onClick={onCancel}>Cancel</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateListForm
+export default CreateListForm;
