fdb1a925d85d6ea6db03251052e6f6d6b9837097
yvonneitan <itan3040@mylaurier.ca>
2024-10-25 11:07:29 -0700

    Add Subcomponents to divide ListsContainer Component

--- client-actrack/src/App.jsx
+++ client-actrack/src/App.jsx
@@ -1,14 +1,10 @@
 import './App.scss';
 import Header from "./components/Header/Header";
-import ListsContainer from './components/ListsContainer/ListsContainer';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ListsPage from './pages/ListsPage/ListsPage';
 import CompletedPage from './pages/CompletedPage/CompletedPage';
 import LoginPage from './pages/LoginPage/LoginPage';
-// import GreetingsContainer from './components/GreetingsContainer/GreetingsContainer';
-// import MiddleContainer from './components/MiddleContainer/MiddleContainer';
-// import RightSideContainer from './components/RightSideContainer/RightSideContainer';
 import ShowRightSideContainer from './components/ShowRightSideContainer/ShowRightSideContainer';
 
 function App() {
@@ -18,7 +14,7 @@ function App() {
       <div className='containers'>
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<><ListsContainer/><HomePage /></>} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/lists" element={<ListsPage />} /> 
           <Route path="/completed" element={<CompletedPage />} />
         </Routes>
