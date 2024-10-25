c4e5c4d1b41ba10fc4a9569f32ac66c8cf344953
yvonneitan <itan3040@mylaurier.ca>
2024-10-25 00:05:46 -0700

    Create a face login page, update the routes

--- client-actrack/src/App.jsx
+++ client-actrack/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.scss'
-import Header from "./components/Header/Header"
-import ListsContainer from './components/ListsContainer/ListsContainer'
-import { BrowserRouter, Routes, Route } from'react-router-dom';
+import './App.scss';
+import Header from "./components/Header/Header";
+import ListsContainer from './components/ListsContainer/ListsContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ListsPage from './pages/ListsPage/ListsPage';
-import CompletedPage from'./pages/CompletedPage/CompletedPage';
+import CompletedPage from './pages/CompletedPage/CompletedPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import GreetingsContainer from './components/GreetingsContainer/GreetingsContainer';
 import MiddleContainer from './components/MiddleContainer/MiddleContainer';
@@ -15,20 +15,16 @@ function App() {
     <BrowserRouter>
       <Header />
       <div className='containers'>
-      <ListsContainer/> 
-      {/* <GreetingsContainer/> */}
-      {/* <MiddleContainer/> */}
-      {/* <RightSideContainer/> */}
-      <RightSideContainer/>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<><ListsContainer/><HomePage /></>} />
+          <Route path="/lists" element={<ListsPage />} /> 
+          <Route path="/completed" element={<CompletedPage />} />
+        </Routes>
+        <RightSideContainer/>
       </div>
-      <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/lists" element={<ListsPage />} />
-      <Route path="/completed" element={<CompletedPage />} />
-      <Route path="/login" element={<LoginPage />} />
-     </Routes>
-     </BrowserRouter>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
--- client-actrack/src/pages/LoginPage/LoginPage.jsx
+++ client-actrack/src/pages/LoginPage/LoginPage.jsx
@@ -1,14 +1,62 @@
-import React from 'react'
-import './LoginPage.scss'
-import ListsContainer from '../../components/ListsContainer/ListsContainer'
+
+import React, { useState } from 'react'; 
+import './LoginPage.scss';
+import userIcon from "../../assets/icons/user.svg";
+import passwordIcon from "../../assets/icons/password.svg";
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const navigate = useNavigate(); 
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // Fake user login credentials
+    const fakeUser = "yvonneitan@example.com";
+    const fakePassword = "password123";
+
+    if (username === fakeUser && password === fakePassword) {
+      setMessage("Login successful!");
+      navigate("/home");
+    } else {
+      setMessage('Login failed: Invalid username or password'); 
+    }
+  };
+
   return (
-    <div className='container'>
-     <ListsContainer/>
+    <div className='login-container'>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className='login-container__user'>
+          <label className='login-container__label'>Username:</label>
+          <img src={userIcon} alt="" className="user__icon" />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            className='login-container__user--name'
+          />
+        </div>
+        <div>
+          <label className='login-container__label'>Password:</label>
+          <img src={passwordIcon} alt="" className="password__icon" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className='login-container__user--password'
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {message && <p>{message}</p>}
     </div>
-  )
+  );
 }
 
-
-export default LoginPage
+export default LoginPage;
