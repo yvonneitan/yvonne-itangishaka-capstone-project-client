32dd06b705fba9e1cfe21924b62315c7f1729a97
yvonneitan <itan3040@mylaurier.ca>
2024-10-25 11:25:00 -0700

    Add Lists to items to the SideBar

--- client-actrack/src/components/ListItem/ListItem.jsx
+++ client-actrack/src/components/ListItem/ListItem.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+function ListItem({ list, onListClick }) {
+    return (
+      <li className="sidebar__lists--item">
+        <button className="sidebar__lists--button" onClick={() => onListClick(list.name)}>
+          {list.name}
+        </button>
+        <span className="sidebar__lists--count">{list.count || 0}</span>
+      </li>
+    );
+  }
+  
+  export default ListItem;
 
-function ListItem() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default ListItem
