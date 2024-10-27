440d7d344f70f4c62c511fcbf4d80f60d1deee77
yvonneitan <itan3040@mylaurier.ca>
2024-10-27 00:37:28 -0700

    Update the Edit list functionality

--- client-actrack/src/components/ListsContainer/ListsContainer.jsx
+++ client-actrack/src/components/ListsContainer/ListsContainer.jsx
@@ -3,7 +3,7 @@ import "./ListsContainer.scss";
 import MiddleContainer from "../MiddleContainer/MiddleContainer";
 import errorIcon from "../../assets/icons/error-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
-import DeleteModal from '../DeleteModal/DeleteModal'; 
+import DeleteModal from "../DeleteModal/DeleteModal";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 
 function ListsContainer({ showForm }) {
@@ -24,12 +24,14 @@ function ListsContainer({ showForm }) {
   const [editedListName, setEditedListName] = useState("");
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const [listToDelete, setListToDelete] = useState(null);
+  const [originalListName, setOriginalListName] = useState(""); 
 
   useEffect(() => {
     const getUser = async () => {
       try {
         const response = await fetch("http://localhost:8080/api/users/1");
-        if (!response.ok) throw new Error("Network response for user was not ok");
+        if (!response.ok)
+          throw new Error("Network response for user was not ok");
 
         const userData = await response.json();
         setUser(userData);
@@ -47,8 +49,11 @@ function ListsContainer({ showForm }) {
 
   const getTaskLists = async (userId) => {
     try {
-      const response = await fetch(`http://localhost:8080/api/lists/task-lists?userId=${userId}`);
-      if (!response.ok) throw new Error("Network response for task lists was not ok");
+      const response = await fetch(
+        `http://localhost:8080/api/lists/task-lists?userId=${userId}`
+      );
+      if (!response.ok)
+        throw new Error("Network response for task lists was not ok");
 
       const data = await response.json();
       setTaskLists(data);
@@ -65,7 +70,6 @@ function ListsContainer({ showForm }) {
     setInputError("");
   };
 
-
   const handleOpenDeleteModal = (taskId) => {
     setListToDelete(taskId);
     setDeleteModalOpen(true);
@@ -74,24 +78,26 @@ function ListsContainer({ showForm }) {
     setDeleteModalOpen(false);
     setListToDelete(null);
   };
-const handleConfirmDelete = async () => {
-  const idToDelete = Number(listToDelete); 
-  console.log('Attempting to delete task list with ID:', idToDelete);
+  const handleConfirmDelete = async () => {
+    const idToDelete = Number(listToDelete);
+    console.log("Attempting to delete task list with ID:", idToDelete);
 
-  try {
-    const response = await fetch(`http://localhost:8080/api/lists/task-lists/${idToDelete}`, {
-      method: 'DELETE',
-    });
-    if (!response.ok) throw new Error('Failed to delete task');
+    try {
+      const response = await fetch(
+        `http://localhost:8080/api/lists/task-lists/${idToDelete}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (!response.ok) throw new Error("Failed to delete task");
 
-    
-    await getTaskLists(user.id);
+      await getTaskLists(user.id);
 
-    handleCloseDeleteModal();
-  } catch (error) {
-    console.error('Error deleting task:', error);
-  }
-};
+      handleCloseDeleteModal();
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
+  };
 
   const handleCreateNewList = async () => {
     if (!newListName.trim()) {
@@ -100,11 +106,14 @@ const handleConfirmDelete = async () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8080/api/lists/task-lists", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name: newListName, userId: user.id }),
-      });
+      const response = await fetch(
+        "http://localhost:8080/api/lists/task-lists",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ name: newListName, userId: user.id }),
+        }
+      );
 
       if (!response.ok) throw new Error("Failed to create new list.");
 
@@ -140,28 +149,20 @@ const handleConfirmDelete = async () => {
     setShowCreateButton(true);
   };
 
-  const handleDeleteList = async (listId) => {
-    const confirmDelete = window.confirm("Are you sure you want to delete this list?");
-    if (!confirmDelete) return;
-
-    try {
-      const response = await fetch(`http://localhost:8080/api/lists/task-lists/${listId}`, {
-        method: "DELETE",
-      });
-      if (!response.ok) throw new Error("Failed to delete the list.");
-
-      await getTaskLists(user.id);
-    } catch (error) {
-      console.error("Error deleting the list:", error);
-    }
-  };
-
   const handleEditList = (listId, listName) => {
     setEditingListId(listId);
     setEditedListName(listName);
+    setOriginalListName(listName); 
   };
 
   const handleEditChange = (e) => setEditedListName(e.target.value);
+
+  const handleCancelEdit = () => {  
+    setEditingListId(null);      
+    setEditedListName(originalListName);
+    setInputError(false);        
+};
+
 
   const handleUpdateList = async () => {
     if (!editedListName.trim()) {
@@ -169,53 +170,62 @@ const handleConfirmDelete = async () => {
       return;
     }
 
+    if (editedListName === originalListName) {
+      handleCancelEdit();
+      return;
+    }
+
     try {
-      const response = await fetch(`http://localhost:8080/api/lists/task-lists/${editingListId}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ name: editedListName }),
-      });
+      const response = await fetch(
+        `http://localhost:8080/api/lists/task-lists/${editingListId}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ name: editedListName }),
+        }
+      );
       if (!response.ok) throw new Error("Failed to update the list.");
 
       await getTaskLists(user.id);
       setEditingListId(null);
       setEditedListName("");
+      setOriginalListName(""); // Clear original name
     } catch (error) {
       console.error("Error updating the list:", error);
     }
   };
   const handleCreateNewTask = async (e) => {
-        e.preventDefault();
-        const newTask = {
-          task: taskName,
-          start_time: startTime,
-          end_time: endTime,
-          list_id: selectedList,
-          user_id: user.id,
-        };
-        try {
-          const response = await fetch("http://localhost:8080/api/tasks", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newTask),
-          });
-    
-          if (!response.ok) {
-            throw new Error("Failed to create new task.");
-          }
-          console.log("res",response.body);
-          const result = await response.json();
-          console.log("New task ID:", result.taskId);
-          console.log("Result",result);
-    
-          getTaskLists(user.id);
-          handleCancelTask(); 
-        } catch (error) {
-          console.error("Error creating new task:", error);
-        }
-      };
+    e.preventDefault();
+    const newTask = {
+      task: taskName,
+      start_time: startTime,
+      end_time: endTime,
+      list_id: selectedList,
+      user_id: user.id,
+    };
+    try {
+      const response = await fetch("http://localhost:8080/api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTask),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to create new task.");
+      }
+      console.log("res", response.body);
+      const result = await response.json();
+      console.log("New task ID:", result.taskId);
+      console.log("Result", result);
+
+      getTaskLists(user.id);
+      handleCancelTask();
+    } catch (error) {
+      console.error("Error creating new task:", error);
+    }
+  };
 
   if (loading) {
     return <div className="sidebar">Loading...</div>;
@@ -237,15 +247,27 @@ const handleConfirmDelete = async () => {
             {taskLists.map((list) => (
               <li key={list.id} className="sidebar__lists--item">
                 {editingListId === list.id ? (
-                  <input
-                    type="text"
-                    value={editedListName}
-                    // onChange={handleEditChange}
-                    onChange={(e) => setEditedListName(e.target.value)}
-
-                    onBlur={handleUpdateList}
-                    className="sidebar__lists--edit-input"
-                  />
+                  <div>
+                    <input
+                      type="text"
+                      value={editedListName}
+                      onChange={handleEditChange}
+                      onBlur={handleUpdateList} 
+                      className="sidebar__lists--edit-input"
+                    />
+                    <button
+                      onClick={handleUpdateList}
+                      className="sidebar__lists--save-button"
+                    >
+                      Save
+                    </button>
+                    <button
+                      onClick={handleCancelEdit}
+                      className="sidebar__lists--cancel-button"
+                    >
+                      Cancel
+                    </button>
+                  </div>
                 ) : (
                   <>
                     <button
@@ -254,14 +276,29 @@ const handleConfirmDelete = async () => {
                     >
                       {list.name}
                     </button>
-                    <span className="sidebar__lists--count">{list.count || 0}</span>
-                     <button onClick={() => handleOpenDeleteModal(list.id)} className="main-content__task--delete-button">
-                  <img src={deleteIcon} alt="edit icon" className="main-content__task--edit-icon"/>
-                  </button>
-                  <button onClick={() => handleEditList(list)} className="main-content__task--edit-button">
-                    <img src={editIcon} alt="edit icon" className="main-content__task--edit-icon"/>
-                  </button>
-                    
+                    <span className="sidebar__lists--count">
+                      {list.count || 0}
+                    </span>
+                    <button
+                      onClick={() => handleOpenDeleteModal(list.id)}
+                      className="main-content__task--delete-button"
+                    >
+                      <img
+                        src={deleteIcon}
+                        alt="delete icon"
+                        className="main-content__task--delete-icon"
+                      />
+                    </button>
+                    <button
+                      onClick={() => handleEditList(list.id, list.name)}
+                      className="main-content__task--edit-button"
+                    >
+                      <img
+                        src={editIcon}
+                        alt="edit icon"
+                        className="main-content__task--edit-icon"
+                      />
+                    </button>
                   </>
                 )}
               </li>
@@ -291,7 +328,10 @@ const handleConfirmDelete = async () => {
                 </div>
               )}
               <div className="sidebar__create--buttons">
-                <button className="sidebar__create--list-add-btn" onClick={handleCreateNewList}>
+                <button
+                  className="sidebar__create--list-add-btn"
+                  onClick={handleCreateNewList}
+                >
                   Add List
                 </button>
                 <button
@@ -313,16 +353,18 @@ const handleConfirmDelete = async () => {
       </div>
       <div className="task-form">
         <MiddleContainer selectedList={selectedList} />
-        
-        {!showTaskForm && showForm!==false && (
+
+        {!showTaskForm && showForm !== false && (
           <button className="task-form__add--task" onClick={handleShowTaskForm}>
             + Add new Task ⌘N
           </button>
         )}
-      
+
         {showTaskForm && (
           <form onSubmit={handleCreateNewTask} className="task-form__container">
-            <label htmlFor="end-time" className="task-form__label">Task:</label>
+            <label htmlFor="end-time" className="task-form__label">
+              Task:
+            </label>
 
             <input
               type="text"
@@ -334,7 +376,9 @@ const handleConfirmDelete = async () => {
               required
             />
 
-            <label htmlFor="start-time" className="task-form__label">Start date and time:</label>
+            <label htmlFor="start-time" className="task-form__label">
+              Start date and time:
+            </label>
             <input
               type="datetime-local"
               id="start-time"
@@ -344,7 +388,9 @@ const handleConfirmDelete = async () => {
               required
             />
 
-            <label htmlFor="end-time" className="task-form__label">End date and time :</label>
+            <label htmlFor="end-time" className="task-form__label">
+              End date and time :
+            </label>
             <input
               type="datetime-local"
               id="end-time"
@@ -353,7 +399,9 @@ const handleConfirmDelete = async () => {
               onChange={(e) => setEndTime(e.target.value)}
               required
             />
-            <label htmlFor="end-time" className="task-form__label">List:</label>
+            <label htmlFor="end-time" className="task-form__label">
+              List:
+            </label>
 
             <select
               id="task-list"
@@ -362,7 +410,9 @@ const handleConfirmDelete = async () => {
               onChange={(e) => setSelectedList(e.target.value)}
               required
             >
-              <option value="">Select a list</option>
+              <option value="" disabled>
+                Select a list
+              </option>
               {taskLists.map((list) => (
                 <option key={list.id} value={list.id}>
                   {list.name}
@@ -370,13 +420,21 @@ const handleConfirmDelete = async () => {
               ))}
             </select>
             <div className="task-form__btn">
-            <button type="submit" className="task-form__btn--submit">Add Task</button>
-            <button type="button" className="task-form__btn--cancel" onClick={handleCancelTask}>Cancel</button>
+              <button type="submit" className="task-form__btn--submit">
+                Add Task
+              </button>
+              <button
+                type="button"
+                className="task-form__btn--cancel"
+                onClick={handleCancelTask}
+              >
+                Cancel
+              </button>
             </div>
-            </form>
+          </form>
         )}
       </div>
-    
+
       <DeleteModal
         isOpen={deleteModalOpen}
         onClose={handleCloseDeleteModal}
